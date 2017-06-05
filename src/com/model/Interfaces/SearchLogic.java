2a203a1cb88e85c4392fa823b87b4b8379315004
assault65535 <814325852@qq.com>
2017-06-05 23:39:04 +0800

    Return more messages when signing in.

--- src/com/model/Interfaces/SearchLogic.java
+++ src/com/model/Interfaces/SearchLogic.java
@@ -3,15 +3,17 @@ package com.model.Interfaces;
 import com.entity.PaperBean;
 import com.entity.QuestionBean;
 
+import java.util.List;
+
 /**
  * Created by Tnecesoc on 2017/6/5.
  */
 public interface SearchLogic {
 
-    PaperBean getPaperBean(String keyword, @Deprecated int sortway);
+    List<PaperBean> getPaperBean(String keyword, @Deprecated int sortway);
 
-    QuestionBean getQuestionBeanByPaperName(String keyword, int sortway);
+    List<QuestionBean> getQuestionBeanByPaperName(String keyword, int sortway);
 
-    QuestionBean getQuestionBeanByPaperId(int id, int sortway);
+    List<QuestionBean> getQuestionBeanByPaperId(int id, int sortway);
 
 }
